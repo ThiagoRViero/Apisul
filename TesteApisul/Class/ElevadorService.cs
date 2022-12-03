c3edd56bea5bd896c1fcc8a3e699c90359f84035
Thiago Rocha Viero <thiagorviero.com>
2022-12-03 02:17:27 -0300

    First Commit

---
+++ TesteApisul/Class/ElevadorService.cs
@@ -0,0 +1,481 @@
+﻿using Newtonsoft.Json;
+using ProvaAdmissionalCSharpApisul;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesteApisul.Serialization;
+
+namespace TesteApisul.Class
+{
+    internal class ElevadorService : IElevadorService
+    {
+        public List<int> andarMenosUtilizado()
+        {
+            //pega todas as respostas e pega apenas o necessário para o método
+            List<Resposta> respostas = pegaRespostas();
+            int[] andares = new int[respostas.Count];
+
+            for (int i = 0; i < andares.Length; i++)
+            {
+                andares[i] = respostas[i].andar;
+            }
+
+            int qntdAndares = 15;
+            var andarMenosUtilizado = new List<int>();
+            int ocorrenciaTemp = 0;
+            int menorOcorrencia = -1;
+
+            //Percorre todos andares, a quantidade de andares deve ser informada na variável qntdAndares
+            for (int i = 0; i <= qntdAndares; i++)
+            {
+                foreach (int a in andares)
+                {
+                    if (i == a)
+                    {
+                        ocorrenciaTemp++;
+                    }
+                }
+                if(menorOcorrencia < 0 )
+                {
+                    menorOcorrencia = ocorrenciaTemp;
+                }
+                if (ocorrenciaTemp <= menorOcorrencia)
+                {
+                    if (!(andarMenosUtilizado.IndexOf(i) >= 0))
+                    {
+                        if (ocorrenciaTemp < menorOcorrencia)
+                        {
+                            andarMenosUtilizado.Clear();
+                        }
+
+                        andarMenosUtilizado.Add(i);
+                        menorOcorrencia = ocorrenciaTemp;
+                    }
+                }
+                ocorrenciaTemp = 0;
+            }
+            return andarMenosUtilizado;
+        }
+
+        public List<char> elevadorMaisFrequentado() //OK
+        {
+            //pega todas as respostas e pega apenas o necessário para o método
+            List<Resposta> respostas = pegaRespostas();
+            char[] elevadores = new char[respostas.Count];
+
+            for (int i = 0; i < elevadores.Length; i++)
+            {
+                elevadores[i] = respostas[i].elevador;
+            }
+
+            //Esta variavel armazenará os elevadores testados diminuindo o processamento
+            var elevadorTestados = new List<char>();
+            var elevadorMaisFrequentado = new List<char>();
+            int ocorrenciaTemp = 0;
+            int ocorrenciaResp = -1;
+
+            //Percorre a lista de elevadores, dentro do for existe outro para efetuar a conta de ocorréncias a lista de elevadores testados impedem o retrabalho
+            for (int i = 0; i < elevadores.Length; i++)
+            {
+                if (!(elevadorTestados.IndexOf(elevadores[i]) >= 0))
+                {
+                    for (int c = 0; c < elevadores.Length; c++)
+                    {
+                        if (elevadores[i] == elevadores[c])
+                        {
+                            ocorrenciaTemp++;
+                        }
+                    }
+                    if (ocorrenciaResp < ocorrenciaTemp)
+                    {
+
+                        elevadorMaisFrequentado.Clear();
+                        elevadorMaisFrequentado.Add(elevadores[i]);
+                        ocorrenciaResp = ocorrenciaTemp;
+                    }
+                    if (ocorrenciaResp == ocorrenciaTemp)
+                    {
+                        if (!(elevadorMaisFrequentado.IndexOf(elevadores[i]) >= 0))
+                        {
+                            elevadorMaisFrequentado.Add(elevadores[i]);
+                            ocorrenciaResp = ocorrenciaTemp;
+                        }
+                    }
+                    ocorrenciaTemp = 0;
+                }
+                elevadorTestados.Add(elevadores[i]);
+            }
+            return elevadorMaisFrequentado;
+        }
+
+        public List<char> periodoMaiorFluxoElevadorMaisFrequentado() //ok
+        {
+            //pega todas as respostas e pega apenas o necessário para o método
+            List<Resposta> respostas = pegaRespostas();
+
+            List<char> elevadorMaisFrequentados = elevadorMaisFrequentado();
+            List<char> turnosMaiorFluxo = new List<char>();
+            List<char> turnos = new List<char>();
+            List<char> listaElevadorComMaiorFrequencia = new List<char>();
+            int ocorrenciaTemp = 0;
+            int ocorrenciaResp = -1;
+
+            
+            foreach (char elevador in elevadorMaisFrequentados)
+            {
+                foreach (Resposta resp in respostas)
+                {
+                    if (elevador == resp.elevador)
+                    {
+                        turnos.Add(resp.turno);
+                    }
+                }
+                for (int c = 0; c < turnos.Count; c++)
+                {
+                    if (turnosMaiorFluxo.IndexOf(turnos[c]) >= 0)
+                    {
+                        continue;
+                    }
+
+                    foreach (char t in turnos)
+                    {
+                        if (turnos[c] == t)
+                        {
+                            ocorrenciaTemp++;
+                        }   
+                    }
+
+                    if (ocorrenciaTemp >= ocorrenciaResp)
+                    {
+                        if (ocorrenciaTemp < ocorrenciaResp)
+                        {
+                            turnosMaiorFluxo.Clear();
+                        }
+                        turnosMaiorFluxo.Add(turnos[c]);
+                        ocorrenciaResp = ocorrenciaTemp;
+                    }
+                    ocorrenciaTemp = 0;
+                }
+                turnosMaiorFluxo.Insert(0, elevador);
+                turnosMaiorFluxo.Add(',');
+                listaElevadorComMaiorFrequencia.AddRange(turnosMaiorFluxo);
+                turnosMaiorFluxo.Clear();
+                ocorrenciaTemp = 0;
+                ocorrenciaResp = -1;
+                turnos.Clear();
+            }
+
+            return listaElevadorComMaiorFrequencia;
+        }
+
+
+        public List<char> elevadorMenosFrequentado() //ok
+        {
+            //pega todas as respostas e pega apenas o necessário para o método
+            List<Resposta> respostas = pegaRespostas();
+            char[] elevadores = new char[respostas.Count];
+
+            for (int i = 0; i < elevadores.Length; i++)
+            {
+                elevadores[i] = respostas[i].elevador;
+            }
+
+            //Variavel armazenará os elevadores testados diminuindo o processamento
+            char[] listaElevadores = { 'A', 'B', 'C', 'D', 'E' };
+            var elevadorMenosFrequentado = new List<char>();
+            int ocorrenciaTemp = 0;
+            int ocorrenciaResp = -1;
+
+            foreach (char c in listaElevadores){
+                foreach (char c2 in elevadores)
+                {
+                    if(c == c2)
+                    {
+                        ocorrenciaTemp++;
+                    }
+                }
+                if (ocorrenciaResp < 0)
+                {
+                    ocorrenciaResp = ocorrenciaTemp;
+                }
+                if(ocorrenciaTemp <= ocorrenciaResp)
+                {
+                    if (ocorrenciaTemp < ocorrenciaResp)
+                    {
+                        elevadorMenosFrequentado.Clear();
+                    }
+                    elevadorMenosFrequentado.Add(c);
+                    ocorrenciaResp = ocorrenciaTemp;
+                }
+                ocorrenciaTemp= 0;
+            }
+
+            return elevadorMenosFrequentado;
+        }
+        public List<char> periodoMenorFluxoElevadorMenosFrequentado()
+        {
+            //pega todas as respostas e pega apenas o necessário para o método
+            List<Resposta> respostas = pegaRespostas();
+            char[] listaTurnos = { 'V', 'M', 'N'};
+
+            List<char> elevadorMenosFrequentados = elevadorMenosFrequentado();//
+            List<char> turnosMenorFluxo = new List<char>();
+            List<char> turnos = new List<char>();//
+            List<char> listaElevadorComMenorFrequencia = new List<char>();
+            int ocorrenciaTemp = 0;
+            int menorOcorrencia = -1;
+
+            foreach (char elevador in elevadorMenosFrequentados)
+            {
+                //Pega todos os turnos deste elevador
+                foreach (Resposta resp in respostas)
+                {
+                    if (elevador == resp.elevador)
+                    {
+                        turnos.Add(resp.turno);
+                    }
+                }
+
+                for(int i = 0; i < listaTurnos.Length; i++)
+                {
+                    //Verifica a existência do turno dentro da lista de saída evitando reprocessamento
+                    if (turnosMenorFluxo.IndexOf(listaTurnos[i]) >= 0)
+                    {
+                        continue;
+                    }
+
+                    foreach (char t in turnos)
+                    {
+                        if (listaTurnos[i] == t)
+                        {
+                            ocorrenciaTemp++;
+                        }
+
+                    }
+
+                    if (menorOcorrencia < 0)
+                    {
+                        menorOcorrencia = ocorrenciaTemp;
+                    }
+
+                    if( ocorrenciaTemp <= menorOcorrencia)
+                        
+                    {
+                        if(ocorrenciaTemp < menorOcorrencia)
+                        {
+                            turnosMenorFluxo.Clear();
+                        }
+                        turnosMenorFluxo.Add(listaTurnos[i]);
+                        menorOcorrencia = ocorrenciaTemp;
+                    }
+                    ocorrenciaTemp = 0;
+                }
+                turnosMenorFluxo.Insert(0, elevador);
+                turnosMenorFluxo.Add(',');
+                listaElevadorComMenorFrequencia.AddRange(turnosMenorFluxo);
+                turnosMenorFluxo.Clear();
+                ocorrenciaTemp = 0;
+                menorOcorrencia = -1;
+                turnos.Clear();
+            }
+
+            return listaElevadorComMenorFrequencia;
+        }
+        public List<char> periodoMaiorUtilizacaoConjuntoElevadores()
+        {
+            //pega todas as respostas e pega apenas o necessário para o método
+            List<Resposta> respostas = pegaRespostas();
+            char[] turnos = new char[respostas.Count];
+
+            for (int i = 0; i < turnos.Length; i++)
+            {
+                turnos[i] = respostas[i].turno;
+            }
+
+            //Variavel armazenará os elevadores testados diminuindo o processamento
+            var turnoTestados = new List<char>();
+            var turnoMaisFrequentado = new List<char>();
+            int ocorrenciaTemp = 0;
+            int ocorrenciaResp = -1;
+
+            for (int i = 0; i < turnos.Length; i++)
+            {
+                if (!(turnoTestados.IndexOf(turnos[i]) >= 0))
+                {
+                    for (int c = 0; c < turnos.Length; c++)
+                    {
+                        if (turnos[i] == turnos[c])
+                        {
+                            ocorrenciaTemp++;
+                        }
+                    }
+                    if (ocorrenciaResp < ocorrenciaTemp)
+                    {
+
+                        turnoMaisFrequentado.Clear();
+                        turnoMaisFrequentado.Add(turnos[i]);
+                        ocorrenciaResp = ocorrenciaTemp;
+                    }
+                    if (ocorrenciaResp == ocorrenciaTemp)
+                    {
+                        if (!(turnoMaisFrequentado.IndexOf(turnos[i]) >= 0))
+                        {
+                            turnoMaisFrequentado.Add(turnos[i]);
+                            ocorrenciaResp = ocorrenciaTemp;
+                        }
+                    }
+                    ocorrenciaTemp = 0;
+                }
+                turnoTestados.Add(turnos[i]);
+            }
+            return turnoMaisFrequentado;
+        }
+
+        public float percentualDeUsoElevadorA()
+        {
+            //pega todas as respostas e pega apenas o necessário para o método
+            List<Resposta> respostas = pegaRespostas();
+            char[] elevadores = new char[respostas.Count];
+            char analisar = 'A';
+            int contador = 0;
+            Decimal porcentagem = 0;
+
+            for (int i = 0; i < elevadores.Length; i++)
+            {
+                elevadores[i] = respostas[i].elevador;
+            }
+
+            foreach (char c in elevadores )
+            {
+                if(c == analisar)
+                {
+                    contador++;
+                }
+            }
+
+            porcentagem = Decimal.Divide(100, elevadores.Length) * contador;
+            return float.Parse(porcentagem.ToString("F2"));
+        }
+
+        public float percentualDeUsoElevadorB()
+        {
+            //pega todas as respostas e pega apenas o necessário para o método
+            List<Resposta> respostas = pegaRespostas();
+            char[] elevadores = new char[respostas.Count];
+            char analisar = 'B';
+            int contador = 0;
+            Decimal porcentagem = 0;
+
+            for (int i = 0; i < elevadores.Length; i++)
+            {
+                elevadores[i] = respostas[i].elevador;
+            }
+
+            foreach (char c in elevadores)
+            {
+                if (c == analisar)
+                {
+                    contador++;
+                }
+            }
+
+            porcentagem = Decimal.Divide(100, elevadores.Length) * contador;
+            return float.Parse(porcentagem.ToString("F2"));
+        }
+
+        public float percentualDeUsoElevadorC()
+        {
+            //pega todas as respostas e pega apenas o necessário para o método
+            List<Resposta> respostas = pegaRespostas();
+            char[] elevadores = new char[respostas.Count];
+            char analisar = 'C';
+            int contador = 0;
+            Decimal porcentagem = 0;
+
+            for (int i = 0; i < elevadores.Length; i++)
+            {
+                elevadores[i] = respostas[i].elevador;
+            }
+
+            foreach (char c in elevadores)
+            {
+                if (c == analisar)
+                {
+                    contador++;
+                }
+            }
+
+            porcentagem = Decimal.Divide(100, elevadores.Length) * contador;
+            return float.Parse(porcentagem.ToString("F2"));
+        }
+
+        public float percentualDeUsoElevadorD()
+        {
+            //pega todas as respostas e pega apenas o necessário para o método
+            List<Resposta> respostas = pegaRespostas();
+            char[] elevadores = new char[respostas.Count];
+            char analisar = 'D';
+            int contador = 0;
+            Decimal porcentagem = 0;
+
+            for (int i = 0; i < elevadores.Length; i++)
+            {
+                elevadores[i] = respostas[i].elevador;
+            }
+
+            foreach (char c in elevadores)
+            {
+                if (c == analisar)
+                {
+                    contador++;
+                }
+            }
+
+            porcentagem = Decimal.Divide(100, elevadores.Length) * contador;
+            return float.Parse(porcentagem.ToString("F2"));
+        }
+
+        public float percentualDeUsoElevadorE()
+        {
+            //pega todas as respostas e pega apenas o necessário para o método
+            List<Resposta> respostas = pegaRespostas();
+            char[] elevadores = new char[respostas.Count];
+            char analisar = 'E';
+            int contador = 0;
+            Decimal porcentagem = 0;
+
+            for (int i = 0; i < elevadores.Length; i++)
+            {
+                elevadores[i] = respostas[i].elevador;
+            }
+
+            foreach (char c in elevadores)
+            {
+                if (c == analisar)
+                {
+                    contador++;
+                }
+            }
+
+            porcentagem = Decimal.Divide(100, elevadores.Length) * contador;
+            return float.Parse(porcentagem.ToString("F2"));
+        }
+
+
+        private List<Resposta> pegaRespostas()
+        {
+            //Pega o caminho principal do projeto para localizar o .JSON
+            String caminho = Directory.GetCurrentDirectory();
+            if (caminho.IndexOf("Debug") >= 0)
+            {
+                caminho = caminho.Replace("\\bin\\Debug", "");
+            }
+            var json = File.ReadAllText(caminho + @"\input.json");
+            var respostas = JsonConvert.DeserializeObject<List<Resposta>>(json);
+            return respostas;
+        }
+    }
+}
---
+++ TesteApisul/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using TesteApisul.Serialization;
+using TesteApisul.Class;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TesteApisul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continuar = true;
+            while (continuar)
+            {
+                Console.WriteLine("Digite 1 para consultar o andar menos utilizado;");
+                Console.WriteLine("Digite 2 para consultar o elevador mais frequentado;");
+                Console.WriteLine("Digite 3 para consultar o elevador mais frequentado e o período que se encontra o maior fluxo;"); ;
+                Console.WriteLine("Digite 4 para consultar o elevador menos frequentado;");
+                Console.WriteLine("Digite 5 para consultar o elevador menos frequentado e o período que se encontra o menor fluxo;");
+                Console.WriteLine("Digite 6 para consultar o período de maior utilização do conjunto de elevadores;");
+                Console.WriteLine("Digite 7 para consultar o percentual de uso dos elevadores;");
+                Console.WriteLine("Digite 0 para sair da aplicação.");
+                ElevadorService ServicoElevador = new ElevadorService();
+                String opcao = Console.ReadLine();
+                Console.Clear();
+                switch (opcao)
+                {
+                    case ("0"):
+                        return;
+                        break;
+                    //Deve retornar uma List contendo o(s) andar(es) menos utilizado(s).
+                    case ("1"):
+                        List<int> andarMenosUtilizado = ServicoElevador.andarMenosUtilizado();
+                        Console.Write("Andar(es) menos utilizado(s): ");
+                        foreach (int andares in andarMenosUtilizado)
+                        {
+                            Console.Write(andares + " ");
+                        }
+                        Console.WriteLine();
+                        break;
+
+                    //Deve retornar uma List contendo o(s) elevador(es) mais frequentado(s)
+                    case ("2"):
+                        List<char> elevadorMaisFrequentado = ServicoElevador.elevadorMaisFrequentado();
+                        Console.Write("Elevador(es) mais frequentado(s): ");
+                        foreach (char elevador in elevadorMaisFrequentado)
+                        {
+                            Console.Write(elevador + " ");
+                        }
+                        break;
+
+                    //Deve retornar uma List contendo o período de maior fluxo de cada um dos elevadores mais frequentado
+                    case ("3"):
+                        List<char> elevadorMaisFrequentadoFluxo = ServicoElevador.periodoMaiorFluxoElevadorMaisFrequentado();
+                        Console.WriteLine("Lista de elevador(es) mais utilizados com o(s) perídos de maior fluxo:");
+                        for (int i = 0; i < elevadorMaisFrequentadoFluxo.Count(); i++)
+                        {
+                            if (i == 0)
+                            {
+                                Console.Write("Elevador: " + elevadorMaisFrequentadoFluxo[i] + " | Período: ");
+                                continue;
+                            }
+                            else if (elevadorMaisFrequentadoFluxo[i - 1].Equals(','))
+                            {
+                                Console.Write("Elevador: " + elevadorMaisFrequentadoFluxo[i] + " | Período: ");
+                                continue;
+                            }
+                            else if (!(elevadorMaisFrequentadoFluxo[i].Equals(',')))
+                            {
+                                Console.Write(elevadorMaisFrequentadoFluxo[i] + " ");
+                                continue;
+                            }
+                            else
+                            {
+                                Console.WriteLine("");
+                                continue;
+                            }
+
+                        }
+
+                        break;
+
+                    //Deve retornar uma List contendo o(s) elevador(es) menos frequentado(s).
+                    case ("4"):
+                        List<char> elevadorMenosFrequentado = ServicoElevador.elevadorMenosFrequentado();
+                        Console.Write("Elevador(es) menos frequentado(s): ");
+                        foreach (char elevador in elevadorMenosFrequentado)
+                        {
+                            Console.Write(elevador + " ");
+                        }
+                        Console.WriteLine();
+                        break;
+
+                    //Deve retornar uma List contendo o período de menor fluxo de cada um dos elevadores menos frequentados (se houver mais de um)..
+                    case ("5"):
+                        List<char> elevadorMenosFrequentadoFluxo = ServicoElevador.periodoMenorFluxoElevadorMenosFrequentado();
+                        Console.WriteLine("Lista de elevador(es) menos utilizados com o(s) perídos de menor fluxo:");
+                        for (int i = 0; i < elevadorMenosFrequentadoFluxo.Count(); i++)
+                        {
+                            if (i == 0)
+                            {
+                                Console.Write("Elevador: " + elevadorMenosFrequentadoFluxo[i] + " | Período: ");
+                                continue;
+                            }
+                            else if (elevadorMenosFrequentadoFluxo[i - 1].Equals(','))
+                            {
+                                Console.Write("Elevador: " + elevadorMenosFrequentadoFluxo[i] + " | Período: ");
+                                continue;
+                            }
+                            else if (!(elevadorMenosFrequentadoFluxo[i].Equals(',')))
+                            {
+                                Console.Write(elevadorMenosFrequentadoFluxo[i] + " ");
+                                continue;
+                            }
+                            else
+                            {
+                                Console.WriteLine("");
+                                continue;
+                            }
+
+                        }
+
+                        break;
+
+                    //Deve retornar uma List contendo o(s) periodo(s) de maior utilização do conjunto de elevadores.
+                    case ("6"):
+                        List<char> turnoMaiorUtilizacao = ServicoElevador.periodoMaiorUtilizacaoConjuntoElevadores();
+                        Console.Write("Turno(s) de maior utilização do conjunto de elevadores: ");
+                        for (int i = 0; i < turnoMaiorUtilizacao.Count(); i++)
+                        {
+                            Console.Write(turnoMaiorUtilizacao[i] + " ");
+
+                        }
+                        break;
+
+                    //Deve retornar um float (duas casas decimais) contendo o percentual de uso do elevador A em relação a todos os serviços prestados.
+                    case ("7"):
+                        float percentual = ServicoElevador.percentualDeUsoElevadorA();
+                        Console.WriteLine("O percentual de uso do elevador A foi de " + percentual +"%");
+                        percentual = ServicoElevador.percentualDeUsoElevadorB();
+                        Console.WriteLine("O percentual de uso do elevador B foi de " + percentual + "%");
+                        percentual = ServicoElevador.percentualDeUsoElevadorC();
+                        Console.WriteLine("O percentual de uso do elevador C foi de " + percentual + "%");
+                        percentual = ServicoElevador.percentualDeUsoElevadorD();
+                        Console.WriteLine("O percentual de uso do elevador D foi de " + percentual + "%");
+                        percentual = ServicoElevador.percentualDeUsoElevadorE();
+                        Console.WriteLine("O percentual de uso do elevador E foi de " + percentual + "%");
+                        break;
+                }
+                Console.ReadLine();
+                Console.Clear();
+            }
+
+        }
+    }
+}
